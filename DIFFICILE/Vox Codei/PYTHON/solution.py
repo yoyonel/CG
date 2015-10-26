1561df687b91ada40c13f832a82385d2e997b31c
ATTY Lionel <lionel.atty@ign.fr>
2015-10-26 19:08:52 +0100

    Vox Codei (Difficile): Update Fini => 100% et optimisation sur les tours rounds. Faut revoir le code et le simplifier, mais normalement on est pas mal là ^^ utilisation d'une méthode de copie (deepcopy) des classes pour sauver les états du solver. Permet par la suite de faire un bon dans le temps pour voir si il y a une meilleur solution, et permet aussi de placer les bombes puis les retirer si la solution n'est pas 'intéressante' (optimale). Reste plus qu'à cleaner le code et ça sera fini pour cet exo !

--- DIFFICILE/Vox Codei/PYTHON/solution.py
+++ DIFFICILE/Vox Codei/PYTHON/solution.py
@@ -1,4 +1,7 @@
 import sys
+from itertools import groupby
+from collections import defaultdict
+import copy
 
 
 class GridForVoxCodei(object):
@@ -31,6 +34,8 @@ class GridForVoxCodei(object):
         self.rows_tiles = []
         #
         self.list_bombs = []
+        #
+        self.gb_on_ca = None
 
     def append_row(self, _row_tiles):
         """
@@ -52,14 +57,15 @@ class GridForVoxCodei(object):
         self.bombs = _bombs
         #
         for tup_bomb in self.list_bombs:
-            row, col, i_round_for_explosion = tup_bomb
-            if i_round_for_explosion == self.rounds:
-                self.do_explosion(row, col)
+            _row, _col, i_round_for_explosion = tup_bomb
+            if i_round_for_explosion >= self.rounds:
+                self.do_explosion(_row, _col)
 
     def do_explosion(self, _i_row, _i_col):
         """
 
-        :param _action:
+        :param _i_row:
+        :param _i_col:
         :return:
         """
         # propagation de l'action de la bombe en horizontal positif
@@ -100,7 +106,8 @@ class GridForVoxCodei(object):
     def prepare_explosion(self, _i_row, _i_col):
         """
 
-        :param _action:
+        :param _i_row:
+        :param _i_col:
         :return:
         """
         # propagation de l'action de la bombe en horizontal positif
@@ -136,49 +143,6 @@ class GridForVoxCodei(object):
             if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
                 if self.rows_tiles[_i_row + offset][_i_col] == self.t_tiles['TILE_NODE']:
                     self.rows_tiles[_i_row + offset][_i_col] = 'T'
-            else:
-                break
-            offset -= 1
-
-    def unprepare_explosion(self, _i_row, _i_col):
-        """
-
-        :param _action:
-        :return:
-        """
-        # propagation de l'action de la bombe en horizontal positif
-        offset = 1
-        while (offset < 4) & ((_i_col + offset) < self.l):
-            if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row][_i_col + offset] == 'T':
-                    self.rows_tiles[_i_row][_i_col + offset] = self.t_tiles['TILE_NODE']
-            else:
-                break
-            offset += 1
-        # propagation de l'action de la bombe en horizontal negatif
-        offset = -1
-        while (offset > -4) & ((_i_col + offset) > -1):
-            if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row][_i_col + offset] == 'T':
-                    self.rows_tiles[_i_row][_i_col + offset] = self.t_tiles['TILE_NODE']
-            else:
-                break
-            offset -= 1
-        # propagation de l'action de la bombe en horizontal positif
-        offset = 1
-        while (offset < 4) & ((_i_row + offset) < self.h):
-            if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row + offset][_i_col] == 'T':
-                    self.rows_tiles[_i_row + offset][_i_col] = self.t_tiles['TILE_NODE']
-            else:
-                break
-            offset += 1
-        # propagation de l'action de la bombe en horizontal negatif
-        offset = -1
-        while (offset > -4) & ((_i_row + offset) > -1):
-            if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row + offset][_i_col] == 'T':
-                    self.rows_tiles[_i_row + offset][_i_col] = self.t_tiles['TILE_NODE']
             else:
                 break
             offset -= 1
@@ -208,6 +172,7 @@ class SolverForVoxCodei(GridForVoxCodei):
         super(SolverForVoxCodei, self).__init__(_l, _h)
 
         self.map_action_bombs = [[0] * width for _ in xrange(height)]
+        self.map_max_connectivities = defaultdict(list)
         #
         self.list_available_cells = []
 
@@ -217,6 +182,7 @@ class SolverForVoxCodei(GridForVoxCodei):
         :return:
         """
         self.map_action_bombs = [[0] * width for _ in xrange(height)]
+        self.map_max_connectivities = defaultdict(list)
 
     def update_influences_of_nodes(self):
         """
@@ -228,18 +194,47 @@ class SolverForVoxCodei(GridForVoxCodei):
                 if tile == self.t_tiles['TILE_NODE']:
                     self.compute_influences_of_nodes(i_row, i_col)
 
+    def compute_max_connectivity_for_cell(self, tag_cell):
+        """
+
+        :param tag_cell:
+        :return:
+        """
+        map_max_connectivities = defaultdict(list)
+        for i_row, row_tiles in enumerate(self.rows_tiles):
+            for i_col, tile in enumerate(row_tiles):
+                if tile == tag_cell:
+                    max_connectivity = self.compute_max_connectivity_for_node(i_row, i_col)
+                    map_max_connectivities[max_connectivity].append((i_row, i_col))
+        return map_max_connectivities
+
+    def update_max_connectivity_for_nodes(self):
+        """
+
+        :return:
+        """
+        self.map_max_connectivities = self.compute_max_connectivity_for_cell(self.t_tiles['TILE_NODE'])
+
+    def compute_max_connectivity_for_nodes_T(self):
+        """
+
+        :return:
+        """
+        return self.compute_max_connectivity_for_cell('T')
+
     def compute_influences_of_nodes(self, _i_row, _i_col):
         """
 
-        :param _action:
+        :param _i_row:
+        :param _i_col:
         :return:
         """
         # propagation de l'action de la bombe en horizontal positif
         offset = 1
         while (offset < 4) & ((_i_col + offset) < self.l):
             if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row][_i_col + offset] is self.t_tiles['TILE_EMPTY']:
-                    self.map_action_bombs[_i_row][_i_col + offset] += 1
+                # if self.rows_tiles[_i_row][_i_col + offset] is self.t_tiles['TILE_EMPTY']:
+                self.map_action_bombs[_i_row][_i_col + offset] += 1
             else:
                 break
             offset += 1
@@ -247,8 +242,8 @@ class SolverForVoxCodei(GridForVoxCodei):
         offset = -1
         while (offset > -4) & ((_i_col + offset) > -1):
             if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row][_i_col + offset] is self.t_tiles['TILE_EMPTY']:
-                    self.map_action_bombs[_i_row][_i_col + offset] += 1
+                # if self.rows_tiles[_i_row][_i_col + offset] is self.t_tiles['TILE_EMPTY']:
+                self.map_action_bombs[_i_row][_i_col + offset] += 1
             else:
                 break
             offset -= 1
@@ -256,8 +251,8 @@ class SolverForVoxCodei(GridForVoxCodei):
         offset = 1
         while (offset < 4) & ((_i_row + offset) < self.h):
             if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row + offset][_i_col] is self.t_tiles['TILE_EMPTY']:
-                    self.map_action_bombs[_i_row + offset][_i_col] += 1
+                # if self.rows_tiles[_i_row + offset][_i_col] is self.t_tiles['TILE_EMPTY']:
+                self.map_action_bombs[_i_row + offset][_i_col] += 1
             else:
                 break
             offset += 1
@@ -265,11 +260,54 @@ class SolverForVoxCodei(GridForVoxCodei):
         offset = -1
         while (offset > -4) & ((_i_row + offset) > -1):
             if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
-                if self.rows_tiles[_i_row + offset][_i_col] is self.t_tiles['TILE_EMPTY']:
-                    self.map_action_bombs[_i_row + offset][_i_col] += 1
+                # if self.rows_tiles[_i_row + offset][_i_col] is self.t_tiles['TILE_EMPTY']:
+                self.map_action_bombs[_i_row + offset][_i_col] += 1
             else:
                 break
             offset -= 1
+
+    def compute_max_connectivity_for_node(self, _i_row, _i_col):
+        """
+
+        :param _i_row:
+        :param _i_col:
+        :return:
+        """
+        # propagation de l'action de la bombe en horizontal positif
+        offset = 1
+        max_value = 0
+        while (offset < 4) & ((_i_col + offset) < self.l):
+            if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
+                max_value = max(max_value, self.map_action_bombs[_i_row][_i_col + offset])
+            else:
+                break
+            offset += 1
+        # propagation de l'action de la bombe en horizontal negatif
+        offset = -1
+        while (offset > -4) & ((_i_col + offset) > -1):
+            if self.rows_tiles[_i_row][_i_col + offset] is not self.t_tiles['TILE_WALL']:
+                max_value = max(max_value, self.map_action_bombs[_i_row][_i_col + offset])
+            else:
+                break
+            offset -= 1
+        # propagation de l'action de la bombe en horizontal positif
+        offset = 1
+        while (offset < 4) & ((_i_row + offset) < self.h):
+            if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
+                max_value = max(max_value, self.map_action_bombs[_i_row + offset][_i_col])
+            else:
+                break
+            offset += 1
+        # propagation de l'action de la bombe en horizontal negatif
+        offset = -1
+        while (offset > -4) & ((_i_row + offset) > -1):
+            if self.rows_tiles[_i_row + offset][_i_col] is not self.t_tiles['TILE_WALL']:
+                max_value = max(max_value, self.map_action_bombs[_i_row + offset][_i_col])
+            else:
+                break
+            offset -= 1
+        #
+        return max_value
 
     def update_list_cells_availables(self):
         """
@@ -316,73 +354,21 @@ class SolverForVoxCodei(GridForVoxCodei):
         self.update_list_cells_availables()
         self.list_bombs.append((_i_row, _i_col, max(0, self.rounds - 3)))
 
-    def unset_bomb_fork(self, _i_row, _i_col):
-        """
-
-        :param _i_row:
-        :param _i_col:
-        :return:
-        """
-        self.rows_tiles[_i_row][_i_col] = self.t_tiles['TILE_EMPTY']
-        self.unprepare_explosion(_i_row, _i_col)
-        self.reset_map_action_bombs()
-        self.update_influences_of_nodes()
-        self.update_list_cells_availables()
-        self.list_bombs.pop()
-
-    def count_ca_to_0connectivity(self):
+    def update_gb_ca_connectivity(self):
         """
 
         :return:
         """
-        return len(
-            [
-                0
-                for tup_coords in self.list_available_cells
-                if (not self.map_action_bombs[tup_coords[0]][tup_coords[1]])
-            ]
-        )
+        self.gb_on_ca = groupby(self.list_available_cells, lambda tup: tup[2])  # groupby on value
 
-    def next_action(self):
+    def __copy__(self):
         """
-
         :return:
         """
-        if bool(self.bombs) & bool(self.list_available_cells):
-            # ###################################################################################
-            # strategie 0: naive, on choisit l'emplacement libre qui detruit le plus de noeuds
-            # ###################################################################################
-            '''
-            self.sort_list_ac_key_0()
-            row, col = self.list_available_cells.pop(-1)
-            self.set_bomb_fork(row, col)
-            return row, col
-            '''
-            # ###################################################################################
-
-            ####################################################################################
-            # strategie 1: pareil que 0 mais on verifie qu'on elimine au moins "un groupe"
-            ####################################################################################
-            ca_0c_before_explosion = self.count_ca_to_0connectivity()
-            print >> sys.stderr, "ca_0c_before_explosion: ", ca_0c_before_explosion
-            self.sort_list_ac_key_0()
-            row, col = 0, 0
-            while self.list_available_cells:
-                row, col, value = self.list_available_cells.pop(-1)
-                print >> sys.stderr, "row, col, value: ", row, col, value
-                self.set_bomb_fork(row, col)
-                ca_0c_after_explosion = self.count_ca_to_0connectivity()
-                print >> sys.stderr, "ca_0c_after_explosion: ", ca_0c_after_explosion
-
-                if ca_0c_after_explosion is not ca_0c_before_explosion:
-                    break
-                self.unset_bomb_fork(row, col)
-            self.set_bomb_fork(row, col)
-            return row, col
-            ####################################################################################
-
-        else:
-            return 'WAIT'
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
 
     def __repr__(self):
         """
@@ -390,8 +376,99 @@ class SolverForVoxCodei(GridForVoxCodei):
         """
         return \
             '\n'.join(map(str, self.rows_tiles)) + '\n' + \
-            '\n'.join([reduce(lambda a, b: str(a) + ' ' + str(b), row) for row in self.map_action_bombs]) + '\n' + \
+            '\n'.join([reduce(lambda a, b: str(a) + ' ' + str(b), _row) for _row in self.map_action_bombs]) + '\n' + \
             ', '.join(map(str, self.list_available_cells))
+
+
+def compute_gb_connectivity(l):
+    """
+
+    :param l:
+    :return:
+    """
+    l_c = defaultdict(list)
+    for _row, _col, value in l:
+        l_c[value].append((_row, _col))
+    return l_c
+
+
+def compute_best_position_for_bomb(solver):
+    """
+
+    :param solver:
+    :return:
+    """
+    # ###################################################################################
+    # strategie 2:
+    # ###################################################################################
+    print >> sys.stderr, "Solver [avant]:\n", solver
+
+    list_connectivities = compute_gb_connectivity(solver.list_available_cells)
+    list_values = sorted(list_connectivities.keys(), reverse=True)
+    print >> sys.stderr, "list_values: ", list_values
+
+    solver.update_max_connectivity_for_nodes()
+    print >> sys.stderr, "solver.map_max_connectivities (avant explosion): ", solver.map_max_connectivities
+    nb_ca_with_c1_before_explosion = len(solver.map_max_connectivities[1])
+    print >> sys.stderr, "nombre de ca avec connectivite 1 (avant explosion): ", nb_ca_with_c1_before_explosion
+
+    # print >> sys.stderr, "list_values: ", list_values
+    _row, _col = 0, 0
+    nb_ca_with_c1_after_explosion = nb_ca_with_c1_before_explosion + 1
+    value = 0
+    for value in list_values:
+        for _row, _col in list_connectivities[value]:
+            # on recupere la position de cellule libre la plus rentable en terme d'explosion
+            print >> sys.stderr, "value: %d -> coord cell: (%d, %d): " % (value, _row, _col)
+
+            # On cree une copie du solver pour effectuer un test de validite du prochain mouvement
+            solver_2 = copy.deepcopy(solver)
+            print >> sys.stderr, "%s %s" % (hex(id(solver)), hex(id(solver_2)))
+            solver_2.set_bomb_fork(_row, _col)
+            solver_2.update_max_connectivity_for_nodes()
+            #nb_ca_with_c1_after_explosion = len(filter(lambda v: v == 1, solver_2.map_max_connectivity.values()))
+            nb_ca_with_c1_after_explosion = len(solver_2.map_max_connectivities[1])
+            print >> sys.stderr, "nombre de ca avec connectivite 1 (apres explosion): ", nb_ca_with_c1_after_explosion
+            if nb_ca_with_c1_after_explosion <= nb_ca_with_c1_before_explosion:
+                break
+        print >> sys.stderr, ""
+        if nb_ca_with_c1_after_explosion <= nb_ca_with_c1_before_explosion:
+            break
+
+    return _row, _col, value
+
+
+def compute_next_move(solver, _state):
+    """
+
+    :param solver:
+    :return:
+    """
+    if _state == 0:
+        if bool(solver.bombs) & bool(solver.list_available_cells):
+            # calcul de la 'meilleur' position pour placer une bombe au prochain tour
+            _row, _col, value = compute_best_position_for_bomb(solver)
+            print >> sys.stderr, "row, col, value: ", _row, _col, value
+
+            if solver.bombs == 1:
+                # il ne reste qu'une bombe
+                # on test pour voir si en attendant on a pas un meilleur coup possible
+                solver_2 = copy.deepcopy(solver)
+                solver_2.update(rounds - 3, bombs)
+                solver_2.update_list_cells_availables()
+                row2, col2, value2 = compute_best_position_for_bomb(solver_2)
+                print >> sys.stderr, "row2, col2, value2: ", row2, col2, value2
+                if value2 > value:
+                    return (row2, col2), 1
+
+            solver.set_bomb_fork(_row, _col)
+
+            return (_row, _col), 0
+        else:
+            return 'WAIT', 0
+    elif _state == 1:
+        return 'WAIT', 1
+
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -407,8 +484,8 @@ for i in xrange(height):
 
 grid.update_influences_of_nodes()
 grid.update_list_cells_availables()
-print >> sys.stderr, "grid:\n", grid
 
+state = 0
 # game loop
 while 1:
     # rounds: number of rounds left before the end of the game
@@ -419,10 +496,20 @@ while 1:
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
 
-    # print "3 0"
-    next_action = grid.next_action()
+    # next_action = grid.next_action()
+    next_action, state = compute_next_move(grid, state)
+    print >> sys.stderr, "next_action, state: ", next_action, state
+
     if next_action == 'WAIT':
         print 'WAIT'
+    elif state == 1:
+        row, col = next_action
+        print >> sys.stderr, "grid.rows_tiles[row][col]: ", grid.rows_tiles[row][col]
+        while grid.rows_tiles[row][col] == 'T':
+            print 'WAIT'
+            rounds, bombs = [int(i) for i in raw_input().split()]
+            grid.update(rounds, bombs)
+        print "%d %d" % (col, row)
+        state = 0
     else:
         print "%d %d" % (next_action[1], next_action[0])
-    print >> sys.stderr, "grid:\n", grid
